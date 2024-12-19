e513821931b194cf452a3e883e7e68d38c1b638c
Sergey <S.Valeev2006@yandex.ru>
2024-12-19 17:06:32 +0300

    добавил метод delegate

--- DZ8/Classes/Task.cs
+++ DZ8/Classes/Task.cs
@@ -6,37 +6,50 @@ using System.Threading.Tasks;
 
 namespace DZ8
 {
-        // Класс представляющий задачу
-        public class Task
+    public class Task
+    {
+        public string Title { get; private set; }
+        public Person Initiator { get; private set; }
+        public Person Assignee { get; private set; }
+        public List<Report> Reports { get; private set; }
+        public TaskStatus Status { get; private set; }
+
+        // Конструктор
+        public Task(string title, Person assignee, Project project)
         {
-            public string Title { get; private set; }
-            public Person Initiator { get; private set; }
-            public Person Assignee { get; private set; }
-            public List<Report> Reports { get; private set; }
-            public TaskStatus Status { get; private set; }
+            Title = title;
+            Assignee = assignee;
+            Initiator = project.TeamLead;
+            Reports = new List<Report>();
+            Status = TaskStatus.Assigned;
+        }
 
-            // Конструктор
-            public Task(string title, Person assignee, Project project)
+        // Назначает задачу исполнителю и меняет статус
+        public void Take(Person person)
+        {
+            Assignee = person;
+            Status = TaskStatus.InProgress;
+        }
+
+        // Добавляет отчет к задаче и меняет статусlj,fdbk 
+        public void AddReport(Report report)
+        {
+            Reports.Add(report);
+            Status = TaskStatus.UnderReview;
+        }
+
+        // Делегирует задачу другому участнику
+        public void Delegate(Person newAssignee)
+        {
+            if (Status == TaskStatus.Assigned || Status == TaskStatus.InProgress)
             {
-                Title = title;
-                Assignee = assignee;
-                Initiator = project.TeamLead;
-                Reports = new List<Report>();
-                Status = TaskStatus.Assigned;
+                Assignee = newAssignee;
+                Console.WriteLine($"Задача '{Title}' делегирована {newAssignee.Name}.");
             }
-
-            // Назначает задачу исполнителю и меняет статус на "В работе"
-            public void Take(Person person)
+            else
             {
-                Assignee = person;
-                Status = TaskStatus.InProgress;
-            }
-
-            // Добавляет отчет к задаче и меняет статус на "На проверке"
-            public void AddReport(Report report)
-            {
-                Reports.Add(report);
-                Status = TaskStatus.UnderReview;
+                Console.WriteLine("Задачу нельзя делегировать, когда она находится в статусе 'На проверке' или 'Завершена'.");
             }
         }
+    }
 }
--- DZ8/Program.cs
+++ DZ8/Program.cs
@@ -9,35 +9,40 @@ namespace DZ8
     //Сделал пояснения для каждого метода и класса чтобы было понятнее 
         public class Program
         {
-            public static void Main(string[] args)
-            { 
-                Person manager = new Person("Камиль");
-                Person teamLead = new Person("Эмиль");
-                Person developer = new Person("Сергей");
+        public static void Main(string[] args)
+        {
+            Person manager = new Person("Камиль");
+            Person teamLead = new Person("Эмиль");
+            Person developer1 = new Person("Сергей");
+            Person developer2 = new Person("Алексей");
+            Person developer3 = new Person("Ирина");
+            Person developer4 = new Person("Ольга");
+            Person developer5 = new Person("Дмитрий");
+            Person developer6 = new Person("Елена");
+            Person tester1 = new Person("Анна");
+            Person tester2 = new Person("Максим");
+            Person tester3 = new Person("Юлия");
 
-                // Создаем проект
-                Project project = new Project("Разработка нового приложения", manager, teamLead);
-                project.SetDeadline(DateTime.Now.AddMonths(1));
-                project.Start();
+            // Создаем проект
+            Project project = new Project("Разработка нового приложения", manager, teamLead);
+            project.SetDeadline(DateTime.Now.AddMonths(1));
+            project.Start();
 
-                // Добавляем задачи
-                project.AddTask("Создание макета", developer);
-                project.AddTask("Разработка бэкенда", developer);
+            // Добавляем задачи
+            project.AddTask("Создание макета", developer1);
+            project.AddTask("Разработка бэкенда", developer2);
+            project.AddTask("Тестирование приложения", tester1);
+            project.AddTask("Создание документации", developer3);
+            project.AddTask("Настройка окружения", developer4);
+            project.AddTask("Разработка фронтенда", developer5);
+            project.AddTask("Интеграция с API", developer6);
+            project.AddTask("Разработка пользовательских тестов", tester2);
+            project.AddTask("Оптимизация производительности", developer1);
+            project.AddTask("Подготовка к релизу", tester3);
 
-                // Выполняем задачи
-                Task task1 = project.Tasks[0];
-                task1.Take(developer);
-                task1.AddReport(new Report("Отчет по макету", "Макет завершен.", developer));
-
-                // Проверяем
-                if (project.IsCompleted())
-                {
-                    project.Close();
-                }
-                else
-                {
-                    Console.WriteLine("Проект еще не завершен.");
-                }
+            // Выполняем задачи
+            Task task1 = project.Tasks[0];
+            task1.Take(developer1);
             }
         }
 }
